f986fec9b49a949e491bf4ffe1e84838aaea66e1
zoubin <zoubin04@gmail.com>
2015-10-28 13:32:00 +0800

    fix option processor, support arguments

--- test/processor.js
+++ test/processor.js
@@ -10,61 +10,55 @@ import postcss from 'postcss'
 var fixtures = path.resolve.bind(path, __dirname, 'fixtures', 'processor')
 
 test('processor', function(t) {
-  let processor = postcss([
-    atImport(),
-    url(),
-    vars(),
-  ])
+  t.plan(1)
   let stream = new Depsify({
     basedir: fixtures(),
-    processor: (result) => {
-      return processor.process(result.css, { from: result.from, to: result.from })
-        .then((res) => {
-          result.css = res.css
-        })
-    },
+    processor,
   })
   stream.write({ file: './import-url.css' })
   stream.end({ file: './import-and-deps.css' })
-  return stream.pipe(sink.obj((rows, done) => {
-    t.same(sort(rows), sort([
-      {
-        deps: {
-          './import-url': fixtures('import-url.css'),
-        },
-        file: fixtures('import-and-deps.css'),
-        id: fixtures('import-and-deps.css'),
-        source: '.import-and-deps {\n  color: #FF0000;\n}\n\n',
-      },
-      {
-        deps: {},
-        file: fixtures('import-url.css'),
-        id: fixtures('import-url.css'),
-        source: '.dialog {\n  background: url(node_modules/sprites/dialog/sp-dialog.png)\n}\n.importUrl{}\n\n',
-      },
-    ]))
-    done()
-  }))
+  run(stream, t)
 })
 
 test('processor, from stream', function(t) {
-  let processor = postcss([
+  t.plan(1)
+  let stream = new Depsify({
+    basedir: fixtures(),
+  })
+  stream.write({ processor })
+  stream.write({ file: './import-url.css' })
+  stream.end({ file: './import-and-deps.css' })
+  run(stream, t)
+})
+
+test('processor, with arguments', function(t) {
+  t.plan(3)
+  let stream = new Depsify({
+    basedir: fixtures(),
+    processor: [[function (result, a, b) {
+      t.same([a, b], [1, 2])
+      return processor(result)
+    }, 1, 2]],
+  })
+  stream.write({ file: './import-url.css' })
+  stream.end({ file: './import-and-deps.css' })
+  run(stream, t)
+})
+
+function processor(result) {
+  let p = postcss([
     atImport(),
     url(),
     vars(),
   ])
-  let stream = new Depsify({
-    basedir: fixtures(),
-  })
-  stream.write({ processor: (result) => {
-    return processor.process(result.css, { from: result.from, to: result.from })
-      .then((res) => {
-        result.css = res.css
-      })
-  } })
-  stream.write({ file: './import-url.css' })
-  stream.end({ file: './import-and-deps.css' })
-  return stream.pipe(sink.obj((rows, done) => {
+  return p.process(result.css, { from: result.from, to: result.from })
+    .then((res) => {
+      result.css = res.css
+    })
+}
+
+function run(stream, t) {
+  stream.pipe(sink.obj((rows, done) => {
     t.same(sort(rows), sort([
       {
         deps: {
@@ -83,7 +77,7 @@ test('processor, from stream', function(t) {
     ]))
     done()
   }))
-})
+}
 
 function sort(rows) {
   rows.sort((a, b) => {
